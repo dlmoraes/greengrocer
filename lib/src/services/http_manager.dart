4c413273c38b9e5c8ea55d043dc9a91cba73abd1
diegomoraes <diegofrost96@gmail.com>
2022-07-30 09:58:24 -0300

    Criando Auth Repository

--- lib/src/services/http_manager.dart
+++ lib/src/services/http_manager.dart
@@ -4,8 +4,8 @@ abstract class HttpMethods {
   static const String post = 'POST';
   static const String get = 'GET';
   static const String put = 'PUT';
-  static const String delete = 'DELETE';
   static const String patch = 'PATCH';
+  static const String delete = 'DELETE';
 }
 
 class HttpManager {
@@ -18,26 +18,31 @@ class HttpManager {
     // Headers da requisição
     final defaultHeaders = headers?.cast<String, String>() ?? {}
       ..addAll({
-        'Content-Type': 'application/json',
+        'content-type': 'application/json',
         'accept': 'application/json',
         'X-Parse-Application-Id': 'g1Oui3JqxnY4S1ykpQWHwEKGOe0dRYCPvPF4iykc',
-        'X-Parse-REST-API-Key': 'rFBKU8tk0m5ZlKES2CGieOaoYz6TgKxVMv8jRIsN'
+        'X-Parse-REST-API-Key': 'rFBKU8tk0m5ZlKES2CGieOaoYz6TgKxVMv8jRIsN',
       });
+
     Dio dio = Dio();
+
     try {
       Response response = await dio.request(
         url,
         options: Options(
-          method: method,
           headers: defaultHeaders,
+          method: method,
         ),
         data: body,
       );
 
+      // Retorno do resultado do backend
       return response.data;
     } on DioError catch (error) {
+      // Retorno do erro do dio request
       return error.response?.data ?? {};
     } catch (error) {
+      // Retorno de map vazio para error generalizado
       return {};
     }
   }

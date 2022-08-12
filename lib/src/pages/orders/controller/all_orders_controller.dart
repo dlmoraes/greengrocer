284afd3df7dd01cb8898d62c59110a455db9597b
diegomoraes <diegofrost96@gmail.com>
2022-08-12 04:28:17 -0300

    Apresentando pedidos

---
+++ lib/src/pages/orders/controller/all_orders_controller.dart
@@ -0,0 +1,36 @@
+import 'package:get/get.dart';
+import 'package:greengrocer/src/models/order_model.dart';
+import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
+import 'package:greengrocer/src/pages/orders/result/orders_result.dart';
+import 'package:greengrocer/src/pages/orders/view/orders_repository.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
+
+class AllOrdersController extends GetxController {
+  List<OrderModel> allOrders = [];
+  final ordersRepository = OrderRepository();
+  final authController = Get.find<AuthController>();
+  final utilsServices = UtilsServices();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getAllOrders();
+  }
+
+  Future<void> getAllOrders() async {
+    OrdersResult<List<OrderModel>> result = await ordersRepository.getAllOrders(
+      userId: authController.user.id!,
+      token: authController.user.token!,
+    );
+
+    result.when(
+      success: (orders) {
+        allOrders = orders;
+        update();
+      },
+      error: (message) {
+        utilsServices.showToast(message: message, isError: true);
+      },
+    );
+  }
+}
--- lib/src/pages/orders/view/orders_tab.dart
+++ lib/src/pages/orders/view/orders_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:greengrocer/src/config/app_data.dart' as appData;
+import 'package:get/get.dart';
+import 'package:greengrocer/src/pages/orders/controller/all_orders_controller.dart';
 import 'package:greengrocer/src/pages/orders/view/components/order_tile.dart';
 
 class OrdersTab extends StatelessWidget {
@@ -11,12 +12,17 @@ class OrdersTab extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pedidos'),
       ),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(16),
-        physics: const BouncingScrollPhysics(),
-        itemBuilder: (_, index) => OrderTile(order: appData.orders[index]),
-        separatorBuilder: (_, index) => const SizedBox(height: 10),
-        itemCount: appData.orders.length,
+      body: GetBuilder<AllOrdersController>(
+        builder: (controller) {
+          return ListView.separated(
+            padding: const EdgeInsets.all(16),
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (_, index) =>
+                OrderTile(order: controller.allOrders[index]),
+            separatorBuilder: (_, index) => const SizedBox(height: 10),
+            itemCount: controller.allOrders.length,
+          );
+        },
       ),
     );
   }
